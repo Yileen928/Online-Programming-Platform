8e3d70e552ce2e03f40e6acf8f35ad7dadc43b8b
Yileen928 <Yileenbrave@gmail.com>
2025-01-13 17:01:34 +0800

    feat: 优化字体加载方式
    1. 使用 styled-components 管理字体加载
    2. 优化字体文件导入方式
    3. 移除重复的字体声明
    4. 调整字体应用样式

--- frontend/src/fonts.js
+++ frontend/src/fonts.js
@@ -1,9 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
+import AaWeiWeiDianZhenTiFont from './fonts/AaWeiWeiDianZhenTi-2.ttf';
 
 export const GlobalFonts = createGlobalStyle`
   @font-face {
     font-family: 'AaWeiWeiDianZhenTi';
-    src: url('./AaWeiWeiDianZhenTi-2.ttf') format('truetype');
+    src: url(${AaWeiWeiDianZhenTiFont}) format('truetype');
     font-weight: normal;
     font-style: normal;
   }
