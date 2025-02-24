dcba38fd208db08d0dcdea983410fac742045618
zhangfufu <zhangfu128zf@gmail.com>
2025-02-24 17:27:48 +0800

    我的更改

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -302,19 +302,6 @@ const Home = () => {
               </Button>
             </div>
           </div>
-          <Button 
-            type="primary" 
-            className="create-button"
-<<<<<<< HEAD
-            onClick={handleCreateProject}
-            disabled={!selectedTemplate || !projectName.trim()}
-          >
-            创建项目
-=======
-          >
-            CODE
->>>>>>> aa23847679fdd9be02ed463b262503b3dbd898e7
-          </Button>
         </div>
       ),
     },
